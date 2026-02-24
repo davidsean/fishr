1d042442a1c2fdbb97e8fbc5d1516e19604083ae
davidsean <david.sean@gmail.com>
2026-02-24 12:55:47 -0500

    add cpue func

--- R/cpue.R
+++ R/cpue.R
@@ -1,5 +1,18 @@
+#' Calculate Catch per unit effort (CPUE)
+#'
+#' Calculate Catch per unit effort (CPUE) as catch divided by effort, optionally adjusting for gear standardization.
+#' 
+#' @param catch Numeric vector of catch
+#' @param effort Numeric vector of effort
+#' @param gear_factor Numeric adjustment for gear standardize
+#'
+#' @returns a numeric, vector of CPUE values
+#' @export
+#'
+#' @examples
+#' cpue(100,10)
+#' cpue(100, 10, gear_factor=0.5)
 cpue <- function(catch, effort, gear_factor = 1) {
     raw_cpue <- catch / effort
-    raw_cpue <- raw_cpue* gear_factor
-    return(raw_cpue)
+    raw_cpue * gear_factor
 }
