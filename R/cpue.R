461004713e9320593e09c1dbc8eca8cfafbb8755
David Sean <david.sean@gmail.com>
2026-02-25 12:49:43 -0500

    add option

--- R/cpue.R
+++ R/cpue.R
@@ -5,7 +5,7 @@
 #' @param catch Numeric vector of catch
 #' @param effort Numeric vector of effort
 #' @param gear_factor Numeric adjustment for gear standardize
-#' @param verbose
+#' @param verbose Logical value to show messages (default is FALSE, use fishr.verbose option to set globally)
 #'
 #' @returns a numeric, vector of CPUE values
 #' @export
@@ -13,7 +13,12 @@
 #' @examples
 #' cpue(100,10)
 #' cpue(100, 10, gear_factor=0.5)
-cpue <- function(catch, effort, gear_factor = 1, verbose = FALSE) {
+cpue <- function(
+  catch,
+  effort,
+  gear_factor = 1,
+  verbose = getOption("fishr.verbose", default = FALSE)
+) {
   if (verbose) {
     message("Processing ", length(catch), " records")
   }
