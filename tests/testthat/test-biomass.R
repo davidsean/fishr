c5ad1b5cad045679c45ff8baa7062a237e60dd04
David Sean <david.sean@gmail.com>
2026-02-25 12:02:22 -0500

    biomass check

---
+++ tests/testthat/test-biomass.R
@@ -0,0 +1,11 @@
+test_that("biomass calculates simple ratio", {
+  expect_equal(biomass_index(cpue = 100, area_swept = 10), 10)
+})
+
+
+test_that("biomass with vectors simple ratio", {
+  expect_equal(
+    biomass_index(cpue = c(100, 200), area_swept = c(10, 20)),
+    c(10, 10)
+  )
+})
